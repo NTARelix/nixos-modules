32ba212df2e608f07ea491f73f58aa94a3678d7d
Kevin Koshiol <1240214+NTARelix@users.noreply.github.com>
2025-07-02 16:46:07 -0500

    cleanup editor config

--- modules/editor/init.lua
+++ modules/editor/init.lua
@@ -29,41 +29,41 @@ vim.opt.signcolumn = "yes"
 vim.opt.colorcolumn = "80"
 
 -- Generic Keybinds
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "format" })
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]], { desc = "delete" })
-vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "paste" })
-vim.keymap.set("n", "<leader>pv", "<cmd>Telescope find_files<cr>", { desc = "find" })
-vim.keymap.set("n", "<leader>pg", "<cmd>Telescope live_grep<cr>", { desc = "grep" })
-vim.keymap.set("n", "<leader>pb", "<cmd>Telescope buffers<cr>", { desc = "buffers" })
-vim.keymap.set("n", "<leader>ph", "<cmd>Telescope help_tags<cr>", { desc = "help_tags" })
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "swap ↓" })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "swap ↑" })
-vim.keymap.set("n", "J", "mzJ`z", { desc = "join" })
-vim.keymap.set("n", "<C-s>", ":w<cr>", { desc = "save" })
-vim.keymap.set("i", "<C-s>", "<esc>:w<cr>gi", { desc = "save" })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "scroll down" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "scroll up" })
-vim.keymap.set("n", "n", "nzzzv", { desc = "next" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "prev" })
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "replace all" })
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format" })
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]], { desc = "Delete" })
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste" })
+vim.keymap.set("n", "<leader>pv", ":NvimTreeToggle<cr>", { desc = "View" })
+vim.keymap.set("n", "<leader>pf", "<cmd>Telescope find_files<cr>", { desc = "Find" })
+vim.keymap.set("n", "<leader>pg", "<cmd>Telescope live_grep<cr>", { desc = "Grep" })
+vim.keymap.set("n", "<leader>pb", "<cmd>Telescope buffers<cr>", { desc = "Buffers" })
+vim.keymap.set("n", "<leader>ph", "<cmd>Telescope help_tags<cr>", { desc = "Help" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Swap ↓" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Swap ↑" })
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join" })
+vim.keymap.set("n", "<C-s>", ":w<cr>", { desc = "Save" })
+vim.keymap.set("i", "<C-s>", "<esc>:w<cr>gi", { desc = "Save" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Prev" })
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace all" })
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<leader><leader>", ":so %<CR>", { desc = "source" })
+vim.keymap.set("n", "<leader><leader>", ":so %<CR>", { desc = "Source" })
 
--- gitsigns
-require("gitsigns").setup()
+-- Plugins
+local gitsigns = require("gitsigns")
+local cmp = require("cmp")
+local lspconfig = require("lspconfig")
+local nvim_tree = require("nvim-tree")
+local nvim_treesitter = require("nvim-treesitter")
+local telescope = require("telescope")
+local which_key = require("which-key")
 
--- nvim-cmp
-require("cmp").setup()
-
--- nvim-tree
-require("nvim-tree").setup()
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-
--- nvim-treesitter
-require("nvim-treesitter").setup()
-
--- telescope
-require("telescope").setup{
+gitsigns.setup()
+cmp.setup()
+nvim_tree.setup()
+nvim_treesitter.setup()
+telescope.setup({
   defaults = {
     mappings = {
       i = {
@@ -73,28 +73,15 @@ require("telescope").setup{
       },
     },
   },
-}
-
--- typescript-language-server
-require('lspconfig').ts_ls.setup({})
-
--- vue-language-server
-require('lspconfig').volar.setup({
+})
+lspconfig.ts_ls.setup({})
+lspconfig.volar.setup({
   filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
   root_dir = require('lspconfig.util').root_pattern('package.json', 'tsconfig.json', 'jsconfig.json', '.git'),
 })
-
---vim.lsp.enable('typescript-language-server')
---vim.lsp.config('typescript-language-server', {
---  settings = {
---    ["typescript-language-server"] = {},
---  },
---})
-
---vim.lsp.enable('vue-language-server')
---vim.lsp.config('vue-language-server', {
---  settings = {
---    ["vue-language-server"] = {},
---  },
---})
+which_key.setup({
+    spec = {
+        { "<leader>p", group = "Project" },
+    },
+})
 
