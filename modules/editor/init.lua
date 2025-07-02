8913d8e4cb0b7e9f31eedb2c18589b53c8576ff9
Kevin Koshiol <1240214+NTARelix@users.noreply.github.com>
2025-07-02 02:52:29 +0000

    copy some of prime's keybinds and options

--- modules/editor/init.lua
+++ modules/editor/init.lua
@@ -7,13 +7,13 @@ vim.opt.listchars = {
   tab = "»·",
   multispace = "·",
   lead = "·",
-  leadmultispace = "|·",
+  leadmultispace = "|···",
   trail = "·",
 }
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.wrap = true
@@ -25,12 +25,29 @@ vim.opt.hidden = true
 vim.opt.incsearch = true
 vim.opt.hlsearch = true
 vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "80"
 
 -- Generic Keybinds
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Find" })
-vim.keymap.set("n", "<leader>p", vim.cmd.Ex, { desc = "Find" })
-vim.keymap.set("n", "<C-s>", ":w<CR>", { desc = "Save" })
-vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>gi", { desc = "Save" })
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "format" })
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]], { desc = "delete" })
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "paste" })
+vim.keymap.set("n", "<leader>pv", "<cmd>Telescope find_files<cr>", { desc = "find" })
+vim.keymap.set("n", "<leader>pg", "<cmd>Telescope live_grep<cr>", { desc = "grep" })
+vim.keymap.set("n", "<leader>pb", "<cmd>Telescope buffers<cr>", { desc = "buffers" })
+vim.keymap.set("n", "<leader>ph", "<cmd>Telescope help_tags<cr>", { desc = "help_tags" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "swap ↓" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "swap ↑" })
+vim.keymap.set("n", "J", "mzJ`z", { desc = "join" })
+vim.keymap.set("n", "<C-s>", ":w<cr>", { desc = "save" })
+vim.keymap.set("i", "<C-s>", "<esc>:w<cr>gi", { desc = "save" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "scroll down" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "scroll up" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "next" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "prev" })
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "replace all" })
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<leader><leader>", ":so %<CR>", { desc = "source" })
 
 -- gitsigns
 require("gitsigns").setup()
@@ -46,10 +63,6 @@ vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent
 require("nvim-treesitter").setup()
 
 -- telescope
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
-vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
-vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
-vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
 require("telescope").setup{
   defaults = {
     mappings = {
