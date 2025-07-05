e4ce0972691bbb4f3ffe8ff47c5f47f66557b8fa
Kevin Koshiol <1240214+NTARelix@users.noreply.github.com>
2025-07-04 21:07:07 -0500

    switch back to default telescope navigation keymap

--- modules/editor/plugins-telescope.lua
+++ modules/editor/plugins-telescope.lua
@@ -2,8 +2,6 @@ require("telescope").setup({
     defaults = {
         mappings = {
             i = {
-                ["<C-j>"] = require("telescope.actions").move_selection_next,
-                ["<C-k>"] = require("telescope.actions").move_selection_previous,
                 ["<Esc>"] = require("telescope.actions").close,
             },
         },
