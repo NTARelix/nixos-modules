f2216bafbd21f59bb565fd2a8b25aec5aa0bb729
Kevin Koshiol <1240214+NTARelix@users.noreply.github.com>
2025-07-07 13:16:39 -0500

    editor automatically adjusts leadmultispace characters

--- modules/editor/options.lua
+++ modules/editor/options.lua
@@ -1,11 +1,4 @@
 vim.opt.list = true
-vim.opt.listchars = {
-    tab = "»·",
-    multispace = "·",
-    lead = "·",
-    leadmultispace = "|···",
-    trail = "·",
-}
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
@@ -23,4 +16,25 @@ vim.opt.hlsearch = true
 vim.opt.termguicolors = true
 vim.opt.signcolumn = "yes"
 vim.opt.colorcolumn = "120"
+vim.opt.listchars = {
+    tab = "»·",
+    multispace = "·",
+    lead = "·",
+    leadmultispace = "|···",
+    trail = "·",
+    nbsp = "⍽",
+}
+local function update_lead()
+    local lc = vim.opt_local.listchars:get()
+    local lms = vim.fn.str2list(lc.leadmultispace)
+    local space = vim.fn.str2list(lc.multispace)
+    local lead = {lms[1]}
+    for i = 1, vim.bo.tabstop - 1 do
+        lead[#lead + 1] = space[i % #space + 1]
+    end
+    vim.opt_local.listchars:append({ leadmultispace = vim.fn.list2str(lead) })
+end
+vim.api.nvim_create_autocmd("OptionSet", { pattern = { "listchars", "tabstop", "filetype" }, callback = update_lead })
+vim.api.nvim_create_autocmd("BufEnter", { callback = update_lead })
+vim.api.nvim_create_autocmd("VimEnter", { callback = update_lead, once = true })
 
