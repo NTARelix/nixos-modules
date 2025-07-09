09926fe8b40e648850f4898d76f6eab097859d40
Kevin Koshiol <1240214+NTARelix@users.noreply.github.com>
2025-07-09 12:02:45 -0500

    remove forced highlighting in editor

--- modules/editor/plugins-nvim-treesitter.lua
+++ modules/editor/plugins-nvim-treesitter.lua
@@ -1,12 +1,2 @@
 require("nvim-treesitter").setup()
-require("nvim-treesitter.configs").setup({
-    modules = {},
-    sync_install = false,
-    ensure_installed = {},
-    ignore_install = {},
-    auto_install = false,
-    highlight = {
-        enable = true,
-    },
-})
 
