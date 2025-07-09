873cbb7a8e9abb194f207013e6fd20606b618fb9
Kevin Koshiol <1240214+NTARelix@users.noreply.github.com>
2025-07-09 09:31:13 -0500

    ensure treesitter highlights syntax

--- modules/editor/plugins-nvim-treesitter.lua
+++ modules/editor/plugins-nvim-treesitter.lua
@@ -1,2 +1,12 @@
 require("nvim-treesitter").setup()
+require("nvim-treesitter.configs").setup({
+    modules = {},
+    sync_install = false,
+    ensure_installed = {},
+    ignore_install = {},
+    auto_install = false,
+    highlight = {
+        enable = true,
+    },
+})
 
