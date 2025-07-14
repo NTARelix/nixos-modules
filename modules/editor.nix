b4eaafa4bc39c7ebeb9881d8e1fde3b6c612973f
Kevin Koshiol <1240214+NTARelix@users.noreply.github.com>
2025-07-14 00:43:17 +0000

    upgrade tsgo

--- modules/editor.nix
+++ modules/editor.nix
@@ -6,12 +6,12 @@
 let
     tsgo = pkgs.buildGoModule {
         pname = "tsgo";
-        version = "unstable-2025-07-07";
+        version = "unstable-2025-07-12";
         src = pkgs.fetchFromGitHub {
             owner = "microsoft";
             repo = "typescript-go";
-            rev = "742fcd2fcddf43cf3b68201538c783f2bf488ea3";
-            sha256 = "sha256-kOLZTSbb7MUbJgMS2Gag85mjwZntS1ZfUUT+K+NTRQQ=";
+            rev = "2b82831a05b6b99da279d12fecbcbc460574a85b";
+            sha256 = "sha256-psDVrF3NNeL+Y6dM6kjDKX+ThIysnzJkxDQjKvAs/9M=";
         };
         vendorHash = "sha256-9gZ1h/rsJ5DEcU8CJGKszE98GzZqfs2ELp1lbXsliYk=";
     };
