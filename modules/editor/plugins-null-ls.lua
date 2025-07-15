ec2572e5b5cb5d249c975542170dcf50646781d9
Kevin Koshiol <1240214+NTARelix@users.noreply.github.com>
2025-07-15 12:39:08 -0500

    remove stylelint from editor

--- modules/editor/plugins-null-ls.lua
+++ modules/editor/plugins-null-ls.lua
@@ -5,11 +5,9 @@ null_ls.setup({
         null_ls.builtins.diagnostics.dotenv_linter,
         null_ls.builtins.diagnostics.hadolint,
         null_ls.builtins.diagnostics.statix,
-        null_ls.builtins.diagnostics.stylelint,
         null_ls.builtins.diagnostics.todo_comments,
         null_ls.builtins.diagnostics.trail_space,
         null_ls.builtins.diagnostics.yamllint,
         null_ls.builtins.formatting.prettierd,
     },
 })
-
