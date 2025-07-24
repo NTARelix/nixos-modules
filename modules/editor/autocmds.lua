8c69d51361d1fca5ab36515ce4264273b88b4361
Kevin Koshiol <1240214+NTARelix@users.noreply.github.com>
2025-07-24 13:47:00 -0500

    describe auto-format autocmd in editor

--- modules/editor/autocmds.lua
+++ modules/editor/autocmds.lua
@@ -1,4 +1,5 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
+    desc = "Format on save",
     group = vim.api.nvim_create_augroup("LspFormatting", { clear = true }),
     callback = function(args)
         local clients = vim.lsp.get_clients({ bufnr = args.bufnr, name = "null-ls" })
