ac6c8823a41a067a31c7c305a81cfb4391e5eb95
Kevin Koshiol <1240214+NTARelix@users.noreply.github.com>
2025-08-29 10:44:53 -0500

    add azure cli to shell

--- modules/shell.nix
+++ modules/shell.nix
@@ -6,9 +6,15 @@
 # Also adds tools for command-line convenience.
 # All plugins are managed here with NixOS.
 { pkgs, ... }:
+let
+    oldPkgs = import (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/42b35e98c168781a208b31b5095bdb0f336b410a.tar.gz";
+    }) {};
+in
 {
     # Packages
     environment.systemPackages = with pkgs; [
+        oldPkgs.azure-cli # oldPkgs because https://github.com/NixOS/nixpkgs/issues/437525
         devenv
         fzf
         nodejs_24
