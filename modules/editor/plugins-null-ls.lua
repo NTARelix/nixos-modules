1dbd1af6262de2a23f2dea40c4fa94b5da7ac018
Kevin Koshiol <1240214+NTARelix@users.noreply.github.com>
2025-09-11 00:52:49 +0000

    only enable prettier formatter when prettier config is present

--- modules/editor/plugins-null-ls.lua
+++ modules/editor/plugins-null-ls.lua
@@ -8,6 +8,29 @@ null_ls.setup({
         null_ls.builtins.diagnostics.todo_comments,
         null_ls.builtins.diagnostics.trail_space,
         null_ls.builtins.diagnostics.yamllint,
-        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.prettierd.with({
+            condition = function(utils)
+                return utils.root_has_file({
+                    ".prettierrc",
+                    ".prettierrc.json",
+                    ".prettierrc.json5",
+                    ".prettierrc.yaml",
+                    ".prettierrc.yml",
+                    ".prettierrc.toml",
+                    ".prettierrc.js",
+                    ".prettierrc.ts",
+                    ".prettierrc.cjs",
+                    ".prettierrc.cts",
+                    ".prettierrc.mjs",
+                    ".prettierrc.mts",
+                    "prettier.config.js",
+                    "prettier.config.ts",
+                    "prettier.config.cjs",
+                    "prettier.config.cts",
+                    "prettier.config.mjs",
+                    "prettier.config.mts",
+                })
+            end,
+        }),
     },
 })
