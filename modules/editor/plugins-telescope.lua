892a062f2e8a9480a5c7b68d879a6b622eb69c55
Kevin Koshiol <1240214+NTARelix@users.noreply.github.com>
2025-11-06 20:04:44 -0600

    restyle telescope

--- modules/editor/plugins-telescope.lua
+++ modules/editor/plugins-telescope.lua
@@ -1,10 +1,13 @@
 require("telescope").setup({
     defaults = {
+        layout_config = {
+            prompt_position = "top",
+        },
         mappings = {
             i = {
                 ["<Esc>"] = require("telescope.actions").close,
             },
         },
+        sorting_strategy = "ascending",
     },
 })
-
