2fa6eb3ac9c1649a25f783f8f148971d5bcd7afc
Kevin Koshiol <1240214+NTARelix@users.noreply.github.com>
2026-04-15 11:30:17 -0500

    add telescope-ui-select plugin to editor
    expriment with using nvim-native package manager

--- modules/editor/plugins-telescope.lua
+++ modules/editor/plugins-telescope.lua
@@ -1,3 +1,10 @@
+vim.pack.add({
+    {
+        src = "https://github.com/nvim-telescope/telescope-ui-select.nvim",
+        rev = "6e51d7da30bd139a6950adf2a47fda6df9fa06d2",
+    },
+})
+
 require("telescope").setup({
     defaults = {
         layout_config = {
@@ -10,4 +17,11 @@ require("telescope").setup({
         },
         sorting_strategy = "ascending",
     },
+    extensions = {
+        ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+        },
+    },
 })
+
+require("telescope").load_extension("ui-select")
