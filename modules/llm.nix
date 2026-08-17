a8644ac89b11d510aa478dd15f41203d74275465
Kevin Koshiol <1240214+NTARelix@users.noreply.github.com>
2026-08-17 08:49:03 -0500

    add mcp servers

--- modules/llm.nix
+++ modules/llm.nix
@@ -2,6 +2,11 @@
 {
   environment.systemPackages = with pkgs; [
     claude-code
+    mcp-language-server
+    mcp-nixos
     pi-coding-agent
+    playwright-mcp
+    sox
+    terraform-mcp-server
   ];
 }
